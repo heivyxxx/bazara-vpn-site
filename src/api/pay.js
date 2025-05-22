8ef45317924ae0404e816843e3625c5837b40c82
heivyxxx <heivyxxx@gmail.com>
2025-05-22 12:28:19 +0300

    ╨Р╨▓╤В╨╛-╨║╨╛╨╝╨╝╨╕╤В 22.05.2025 12:28:19 | #424

--- src/api/pay.js
+++ src/api/pay.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
       orderId: order_id,
       successRedirectUrl: "https://bazara-vpn-site.vercel.app/success",
       failRedirectUrl: "https://bazara-vpn-site.vercel.app/fail",
-      expirationDateTime: new Date(Date.now() + 3*60*60*1000).toISOString()
+      expirationDateTime: "2025-05-24T12:00:00.000Z"
     };
     console.log('WATA BODY:', wataBody);
 

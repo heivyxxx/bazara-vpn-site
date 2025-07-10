ea0887d708a08e51daaf05236e108843a43d34dd
heivyxxx <heivyxxx@gmail.com>
2025-07-10 10:31:27 +0300

    Update page.tsx

--- src/app/admin/support/page.tsx
+++ src/app/admin/support/page.tsx
@@ -2,10 +2,6 @@
 import { useEffect, useState, useRef, useCallback } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from '@/lib/supabaseClient';
-import { createClient } from '@supabase/supabase-js';
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 interface Message {
   author: string;
